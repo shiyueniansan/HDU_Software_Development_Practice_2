deebf3ed6c1f7e865463b8446344a43e72d86445
Keven <2725765146@qq.com>
2023-07-01 17:21:03 +0800

    fix: move derectory "financial" out

---
+++ src/main/java/com/rk/financial/controller/PayController.java
@@ -0,0 +1,58 @@
+package com.rk.financial.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.rk.framework.aspectj.lang.annotation.Log;
+import com.rk.framework.web.controller.BaseController;
+import com.rk.framework.aspectj.lang.enums.BusinessType;
+import com.rk.financial.domain.Pay;
+import com.rk.financial.service.IPayService;
+import com.rk.common.utils.poi.ExcelUtil;
+import com.rk.framework.web.page.TableDataInfo;
+
+/**
+ * 工资表Controller
+ *
+ * @author Keven
+ * @date 2023-05-31
+ */
+@RestController
+@RequestMapping("/financial/pay")
+public class PayController extends BaseController
+{
+    @Autowired
+    private IPayService payService;
+
+    /**
+     * 查询工资表列表
+     */
+    @PreAuthorize("@ss.hasPermi('financial:pay:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(Pay pay)
+    {
+        startPage();
+        List<Pay> list = payService.selectPayList(pay);
+        payService.calculatePay(list);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出工资表列表
+     */
+    @PreAuthorize("@ss.hasPermi('financial:pay:export')")
+    @Log(title = "工资表", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, Pay pay)
+    {
+        List<Pay> list = payService.selectPayList(pay);
+        payService.calculatePay(list);
+        ExcelUtil<Pay> util = new ExcelUtil<Pay>(Pay.class);
+        util.exportExcel(response, list, "工资表数据");
+    }
+}
---
+++ src/main/java/com/rk/financial/service/impl/TitleServiceImpl.java
@@ -0,0 +1,93 @@
+package com.rk.financial.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.rk.financial.mapper.TitleMapper;
+import com.rk.financial.domain.Title;
+import com.rk.financial.service.ITitleService;
+
+/**
+ * 职称Service业务层处理
+ *
+ * @author Keven
+ * @date 2023-05-30
+ */
+@Service
+public class TitleServiceImpl implements ITitleService
+{
+    @Autowired
+    private TitleMapper titleMapper;
+
+    /**
+     * 查询职称
+     *
+     * @param id 职称主键
+     * @return 职称
+     */
+    @Override
+    public Title selectTitleById(Long id)
+    {
+        return titleMapper.selectTitleById(id);
+    }
+
+    /**
+     * 查询职称列表
+     *
+     * @param title 职称
+     * @return 职称
+     */
+    @Override
+    public List<Title> selectTitleList(Title title)
+    {
+        return titleMapper.selectTitleList(title);
+    }
+
+    /**
+     * 新增职称
+     *
+     * @param title 职称
+     * @return 结果
+     */
+    @Override
+    public int insertTitle(Title title)
+    {
+        return titleMapper.insertTitle(title);
+    }
+
+    /**
+     * 修改职称
+     *
+     * @param title 职称
+     * @return 结果
+     */
+    @Override
+    public int updateTitle(Title title)
+    {
+        return titleMapper.updateTitle(title);
+    }
+
+    /**
+     * 批量删除职称
+     *
+     * @param ids 需要删除的职称主键
+     * @return 结果
+     */
+    @Override
+    public int deleteTitleByIds(Long[] ids)
+    {
+        return titleMapper.deleteTitleByIds(ids);
+    }
+
+    /**
+     * 删除职称信息
+     *
+     * @param id 职称主键
+     * @return 结果
+     */
+    @Override
+    public int deleteTitleById(Long id)
+    {
+        return titleMapper.deleteTitleById(id);
+    }
+}
